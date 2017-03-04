096cd1f1d7a1d3d0f7a9c2ebca03955002dd27b1
Galad_Loth <jlfeng911@gmail.com>
2017-03-04 10:09:55 +0800

    Add triplet loss layer

--- net_symbol/custom_layers.py
+++ net_symbol/custom_layers.py
@@ -38,3 +38,75 @@ class DeepCompareLossLayer(mx.operator.NumpyOp):
         dx[:]=1.0
         dx[l>0]=-1.0
         dx[y<0]=0.0
+        
+class TripletLossLayer(mx.operator.NumpyOp):
+    def __init__(self, margin):
+        super(TripletLossLayer, self).__init__(False)
+        self.margin=margin
+        
+    def list_arguments(self):
+        return ['data0','data1','data2']
+        
+    def list_outputs(self):
+        return ['triplet_loss']
+        
+    def infer_shape(self, in_shape):
+        data_shape0=in_shape[0]
+        data_shape1=in_shape[1]
+        data_shape2=in_shape[2]
+        if (data_shape0!=data_shape1) or (data_shape0!=data_shape2):
+            raise ValueError("Shape of inputs does not match:"
+                              "{}{}{}".format(data_shape0, data_shape1,data_shape2))            
+        
+        return [data_shape0, data_shape1,data_shape2],[data_shape0[0]]
+        
+    def forward(self, in_data, out_data):
+        x0=in_data[0]
+        x1=in_data[1]
+        x2=in_data[2]
+        y=out_data[0]
+        d1=x0-x1
+        d2=x0-x2
+        d1=d1*d1
+        d2=d2*d2
+        loss=d1-d2+self.margin
+        y[:]=loss
+        y[loss<0]=0
+#        y[:]=npy.ones((x.shape[0],1))-l.reshape((x.shape[0],1))*x  
+        
+    def backward(self, out_grad, in_data, out_data, in_grad):
+        x0=in_data[0]
+        x1=in_data[1]
+        x2=in_data[2]
+        y=out_data[0]
+        dx0=in_grad[0]
+        dx1=in_grad[1]
+        dx2=in_grad[2]
+        dx0[:]=2*(x2-x1)
+        dx1[:]=2*(x1-x0)
+        dx2[:]=2*(x0-x2)
+        dx0[y<=0]=0
+        dx1[y<=0]=0
+        dx2[y<=0]=0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
